9b61f67b835eb7634af1459fa0e20d2aafce12dd
milad-raza <miladrazabhatti@gmail.com>
2024-10-29 13:29:41 +0500

    telegram full screen support added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp;
+
+      if (typeof tg.requestView === 'function') {
+        tg.requestView('full');
+      } else {
+        console.warn('requestView method is not available in the Telegram WebApp API');
+      }
+      tg.expand();
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mini App
+          Welcome to My Telegram Mini-App
         </p>
       </header>
     </div>
