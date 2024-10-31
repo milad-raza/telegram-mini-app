37caa86ef40fb9356c66a92f4bae330fa82daf94
meelad-blockapex <meelad.raza@blockapex.io>
2024-10-31 13:44:55 +0500

    telegram sdk telegram-ui added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@telegram-apps/telegram-ui/dist/styles.css';
 
-function App() {
+import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
 
-  useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const tg = window.Telegram.WebApp;
-
-      if (typeof tg.requestView === 'function') {
-        tg.requestView('full');
-      } else {
-        console.warn('requestView method is not available in the Telegram WebApp API');
-      }
-
-      tg.expand();
-
-      tg.MainButton.show();
-    }
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to My Telegram Mini-App
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <AppRoot>
+    <Placeholder
+      header="Title"
+      description="Description"
+    >
+      <img
+        alt="Telegram sticker"
+        src="https://xelene.me/telegram.gif"
+        style={{ display: 'block', width: '144px', height: '144px' }}
+      />
+    </Placeholder>
+  </AppRoot>
+);
 
 export default App;
