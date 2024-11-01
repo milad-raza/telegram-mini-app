f020467df7e438f552515882e75bfc36a5a9c792
milad-raza <miladrazabhatti@gmail.com>
2024-11-01 11:24:09 +0500

    telegram expand added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
-import '@telegram-apps/telegram-ui/dist/styles.css';
+// import '@telegram-apps/telegram-ui/dist/styles.css';
 
-import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
+// import { AppRoot, Placeholder } from '@telegram-apps/telegram-ui';
 
-const App = () => (
-  <AppRoot>
-    <Placeholder
-      header="Title"
-      description="Description"
-    >
-      <img
-        alt="Telegram sticker"
-        src="https://xelene.me/telegram.gif"
-        style={{ display: 'block', width: '144px', height: '144px' }}
-      />
-    </Placeholder>
-  </AppRoot>
-);
+// const App = () => (
+//   <AppRoot>
+//     <Placeholder
+//       header="Title"
+//       description="Description"
+//     >
+//       <img
+//         alt="Telegram sticker"
+//         src="https://xelene.me/telegram.gif"
+//         style={{ display: 'block', width: '144px', height: '144px' }}
+//       />
+//     </Placeholder>
+//   </AppRoot>
+// );
+
+// export default App;
+
+
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  function expand (){
+    const tg = window?.Telegram?.WebApp;
+    try {
+      tg.Telegram.WebApp.ready()
+      tg.Telegram.WebApp.expand()
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    // if (window.Telegram && window.Telegram.WebApp) {
+    //   const tg = window.Telegram.WebApp;
+
+    //   if (typeof tg.requestView === 'function') {
+    //     tg.requestView('full');
+    //   } else {
+    //     console.warn('requestView method is not available in the Telegram WebApp API');
+    //   }
+    //   tg.expand();
+    // }
+    expand()
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to My Telegram Mini-App
+        </p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
